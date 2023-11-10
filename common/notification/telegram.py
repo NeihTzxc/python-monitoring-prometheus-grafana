2153e17072a8da04d943fc0d4d1d2b2aea55f709
Thien Nguyen <ngocthienTCVan.b10@gmail.com>
2023-11-10 08:36:45 +0700

    add disable_web_page_preview

--- common/notification/telegram.py
+++ common/notification/telegram.py
@@ -1,10 +1,11 @@
 import os
+import time
 
 import dotenv
 import requests
 
 dotenv.load_dotenv("common/notification/.env")
-
+disable_preview = True
 
 class Telegram:
     def __init__(self):
@@ -14,7 +15,9 @@ class Telegram:
     def send_message(self, message):
         api_url = f'https://api.telegram.org/bot{self.api_key}/sendMessage'
         try:
-            response = requests.post(api_url, json={'chat_id': self.chat_id, 'text': message})
+            start_time = time.time()
+            response = requests.post(api_url, json={'chat_id': self.chat_id, 'text': message, 'disable_web_page_preview': disable_preview})
+            print(f"Execution Time: {time.time() - start_time} seconds")
             print(response.text)
         except Exception as e:
             print(e)
